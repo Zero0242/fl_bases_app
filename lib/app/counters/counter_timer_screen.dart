9f5af95e353da49935b2a40a39a6a9ebfaac251a
Zero0242 <pedrocorream66@gmail.com>
2025-04-08 00:17:08 -0400

    [TEST]
    > pantallas de conteo

---
+++ lib/app/counters/counter_timer_screen.dart
@@ -0,0 +1,56 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+class CounterTimerScreen extends StatefulWidget {
+  const CounterTimerScreen({super.key});
+
+  /// App Route is: `/counter/index`
+  static const String route = '/counter/timer';
+
+  @override
+  State<CounterTimerScreen> createState() => _CounterTimerScreenState();
+}
+
+class _CounterTimerScreenState extends State<CounterTimerScreen> {
+  int _counter = 0;
+  Timer? _timer;
+
+  void _toggleTimer() {
+    if (_timer?.isActive ?? false) {
+      _timer?.cancel();
+    } else {
+      _timer = Timer.periodic(Duration(seconds: 1), (ts) {
+        _counter = _counter + ts.tick;
+        print(_counter);
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text('Contador con Timer'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('You have waite $_counter seconds'),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _toggleTimer,
+        tooltip: 'Conteo',
+        child: Icon(_timer?.isActive ?? false ? Icons.alarm_off : Icons.alarm),
+      ),
+    );
+  }
+}
--- lib/app/home_screen.dart
+++ lib/app/home_screen.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
-import 'counters/counter_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
-  /// App Route is: `/home`
-  static const String route = '/home';
+  /// App Route is: `/`
+  static const String route = '/';
 
   @override
   Widget build(BuildContext context) {
@@ -17,6 +16,36 @@ class HomeScreen extends StatelessWidget {
           Divider(),
           ListTile(
             title: Text('Contador'),
+            onTap: () {},
+            trailing: Icon(Icons.chevron_right),
+          ),
+          ListTile(
+            title: Text('Contador 10'),
+            onTap: () {},
+            trailing: Icon(Icons.chevron_right),
+          ),
+          ListTile(
+            title: Text('Contador 25'),
+            onTap: () {},
+            trailing: Icon(Icons.chevron_right),
+          ),
+          ListTile(
+            title: Text('Contador 50'),
+            onTap: () {},
+            trailing: Icon(Icons.chevron_right),
+          ),
+          ListTile(
+            title: Text('Contador 75'),
+            onTap: () {},
+            trailing: Icon(Icons.chevron_right),
+          ),
+          ListTile(
+            title: Text('Contador 10 25 50 75 100'),
+            onTap: () {},
+            trailing: Icon(Icons.chevron_right),
+          ),
+          ListTile(
+            title: Text('Contador con timer'),
             onTap: () {},
             trailing: Icon(Icons.chevron_right),
           ),
